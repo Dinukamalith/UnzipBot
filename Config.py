715eb91ee444a7ed7e7850453e7f3d8fe69f583d
Dinukamalith <87223443+Dinukamalith@users.noreply.github.com>
2021-10-15 11:33:30 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("8651408", 0))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("6813e013e48bfe4bd007304dcbaf7ade", None)  # Change None to your API_HASH
-    BOT_TOKEN = os.environ.get("2070967057:AAEcBJNgitjhlr04LdgetPa7JMdg5qF6zz8", None)  # Change None to your BOT_TOKEN
-    OWNER_ID = int(os.environ.get("1662123902", 0))  # Change 0 to your OWNER_ID
-    OWNER_NAME = os.environ.get("szbotcreater", None)  # Change None to your OWNER_NAME
+    API_ID = int(os.environ.get("API_ID", 0))  # Change 12345 to your API_ID
+    API_HASH = os.environ.get("API_HASH", None)  # Change None to your API_HASH
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)  # Change None to your BOT_TOKEN
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))  # Change 0 to your OWNER_ID
+    OWNER_NAME = os.environ.get("OWNER_NAME", None)  # Change None to your OWNER_NAME
 
     # For Local Deploys edit above 5 lines.
     # Put your API_ID and OWNER_ID (without comma) and API_HASH,BOT_TOKEN n OWNER_NAME (with commas) below.
